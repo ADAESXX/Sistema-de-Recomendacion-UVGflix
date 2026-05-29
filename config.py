f022e4de6e648cd91d678b26479dc6a9ad2664c6
Abigail Escobar <esc25862@uvg.edu.gt>
2026-05-28 20:10:06 -0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+#Nombre del archivo: config.py
+#Autores:
+    #Abigail Escobar
+#Fecha: 27/05/2026
+#Descripcion: configuracion para conectar con neo4j
+
+url = "bolt://127.0.0.1:7687"
+user = "neo4j"
+password = "uvgflixproyecto"
+databajeNeo4j = "neo4j"
